ca5140af140a4ee1fa00b61d7836e3a5692a96b4
Maor <Maor@MAOR-PC>
2022-06-02 14:14:54 +0300

    Add messages_for_spesific_receiver

--- MessagingSystem/views.py
+++ MessagingSystem/views.py
@@ -4,19 +4,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-@api_view(['GET', 'POST'])
-def all_messages(request):
+@api_view(['POST'])
+def new_message(request):
 
-    if request.method == 'GET':
-        messages = Message.objects.all()
-        serializer = MessageSerializer(messages, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = MessageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    serializer = MessageSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'DELETE'])
 def message_detail(request, id):
@@ -33,3 +27,12 @@ def message_detail(request, id):
     elif request.method == 'DELETE':
         message.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def messages_for_spesific_receiver(request, receiver):
+    try:
+        messages = Message.objects.filter(receiver__exact=receiver)
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+    except Message.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
