7e6a19592b5a6e52b6e2576fd43bf9dfc5c90616
Maor <Maor@MAOR-PC>
2022-06-06 18:04:39 +0300

    Rename the urls path

--- MessagingSystem/urls.py
+++ MessagingSystem/urls.py
@@ -5,7 +5,7 @@ from MessagingSystem import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('messages/', views.new_message),
-    path('messages/byId/', views.message_detail),
-    path('messages/all/', views.messages_for_spesific_receiver),
-    path('messages/unread/', views.unread_messages_for_spesific_receiver)
+    path('messages/get_by_id/', views.message_detail),
+    path('messages/get_all/', views.messages_for_spesific_receiver),
+    path('messages/get_all_unread/', views.unread_messages_for_spesific_receiver)
 ]
