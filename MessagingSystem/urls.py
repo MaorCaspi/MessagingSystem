a8f4af5e7080fb02c51ece068be4dfe99f4f78a5
Maor <Maor@MAOR-PC>
2022-06-06 15:54:09 +0300

    Update url parameters

--- MessagingSystem/urls.py
+++ MessagingSystem/urls.py
@@ -5,7 +5,7 @@ from MessagingSystem import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('messages/', views.new_message),
-    path('messages/<int:id>', views.message_detail),
-    path('messages/receiver=<receiver>', views.messages_for_spesific_receiver),
-    path('messages/unread_receiver=<receiver>', views.unread_messages_for_spesific_receiver)
+    path('messages/?id=<int:id>', views.message_detail),
+    path('messages/?receiver=<receiver>', views.messages_for_spesific_receiver),
+    path('messages/?unread_receiver=<receiver>', views.unread_messages_for_spesific_receiver)
 ]
